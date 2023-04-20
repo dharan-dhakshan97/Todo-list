68e3f720c1eb762c6acb8105a18c020ceaebc552
dharan-dhakshan97 <dharanidhakshan97@gmail.com>
2023-04-20 13:27:28 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import Component from "./Todo-list/Component";
 import "./Todo-list/Styles.css";
-const App = () => {
+const App = (props) => {
   return (
     <>
     <Component/>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     </>
   )
 }
